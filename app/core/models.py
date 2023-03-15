d3998502bbcda49a549369b577272c2c01d92be0
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-03-15 18:36:00 +0200

    update recipe model to support images

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,7 @@
 """Database Models"""
+import uuid
+import os
+
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
@@ -7,6 +10,17 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+def recipe_image_file_path(instance, filename):
+    """Generate filepath for new recipe image"""
+
+    # get file extension
+    ext = os.path.splitext(filename)[1]
+
+    # create random uuid filename
+    filename = f'{uuid.uuid4()}{ext}'
+
+    # return path
+    return os.path.join('uploads', 'recipe', filename)
 
 class UserManager(BaseUserManager):
     """Manager for users"""
@@ -59,6 +73,7 @@ class Recipe(models.Model):
     fat = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField(blank=True)
     ingredients = models.TextField(blank=True)
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
