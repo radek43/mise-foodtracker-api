c29c101c5e7d8d2ea0f7fd910d2e591f840eec7a
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-03-19 17:19:51 +0200

    changed permissions for uploading a picture to a recipe

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -43,8 +43,11 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(recipe, data=request.data)
 
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            if request.user.is_staff == True:
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_403_FORBIDDEN)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
