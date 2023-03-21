d11611292ab54f5c560f8a12fc9fe85e54de0243
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-03-21 09:28:33 +0200

    created viewSet for the food API

--- app/food/views.py
+++ app/food/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+"""Views for the food APIs"""
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from auth import custom_permissions
+
+from core.models import Food
+
+from food import serializers
+
+
+class FoodViewSet(viewsets.ModelViewSet):
+    """View for manage food APIs"""
+    serializer_class = serializers.FoodDetailSerializer
+    queryset = Food.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [custom_permissions.UserPermission]
+
+    def get_queryset(self):
+        """Retrieve recipes for authenticated user"""
+        return self.queryset.order_by('-id')
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return serializers.FoodSerializer
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create new food"""
+        serializer.save(user=self.request.user)
