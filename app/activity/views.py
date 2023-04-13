1ce9ca59877efb7c3b867295b28680bbde0094d8
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-04-13 14:11:28 +0300

    added view for activity api

--- app/activity/views.py
+++ app/activity/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+"""Views for Activity API"""
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from auth import custom_permissions
+
+from core.models import Activity
+from activity import serializers
+
+
+class ActivityViewSet(viewsets.ModelViewSet):
+    """View for managing activity APIs"""
+    serializer_class = serializers.ActivitySerializer
+    queryset = Activity.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [custom_permissions.UserPermission]
+
+    def get_queryset(self):
+        """Retrieve activities"""
+        return self.queryset.order_by('-id')
