0171d1a7a23a7281cd9adc4ecfdf923f91331e20
Radu Bila <57503745+radek43@users.noreply.github.com>
2023-04-13 16:12:35 +0300

    added tests to create update and delete activity

--- app/activity/tests/test_activity_api.py
+++ app/activity/tests/test_activity_api.py
@@ -16,7 +16,12 @@ from activity.serializers import (
 
 ACTIVITIES_URL = reverse('activity:activity-list')
 
-def create_activity(user, *params):
+def detail_url(activity_id):
+    """Create and return an activity detail URL"""
+    return reverse('activity:activity-detail', args=[activity_id])
+
+
+def create_activity(user, **params):
     """Create and return a sample activity"""
     defaults = {
         'title': 'Alergare',
@@ -43,8 +48,9 @@ class PrivateActivityAPITests(TestCase):
     def setUp(self):
         self.client = APIClient()
         self.user = get_user_model().objects.create_user(
-            'user@example.com',
-            'testpass123',
+            email='user@example.com',
+            password='test1234',
+            is_staff=True
         )
         self.client.force_authenticate(self.user)
 
@@ -59,3 +65,101 @@ class PrivateActivityAPITests(TestCase):
         serializer = ActivitySerializer(activities, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+
+    def test_get_activity_detail(self):
+        """Test get activity detail"""
+        activity = create_activity(user=self.user)
+
+        url = detail_url(activity.id)
+        res = self.client.get(url)
+
+        serializer = ActivitySerializer(activity)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_create_activity(self):
+        """Test create an activity"""
+        payload = {
+            "title": "Alergare",
+            "met": Decimal('2.4'),
+        }
+        res = self.client.post(ACTIVITIES_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        activity = Activity.objects.get(id=res.data['id'])
+        for k, v in payload.items():
+            self.assertEqual(getattr(activity, k), v)
+        self.assertEqual(activity.user, self.user)
+
+    def test_partial_update(self):
+        """Test update of a activity"""
+        original_met = Decimal('4.3')
+        activity = create_activity(
+            user=self.user,
+            title='Aerobic',
+            met=original_met,
+        )
+
+        payload = {
+            'title': 'Pilates',
+        }
+        url = detail_url(activity.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        activity.refresh_from_db()
+        self.assertEqual(activity.title, payload['title'])
+        self.assertEqual(activity.met, original_met)
+        self.assertEqual(activity.user, self.user)
+
+
+    def test_full_update(self):
+        """Test update of a activity"""
+        activity = create_activity(
+            user=self.user,
+            title='Aerobic',
+            met=Decimal('4.3'),
+        )
+
+        payload = {
+            'title': 'Plimbare',
+            'met': Decimal('2.7'),
+        }
+        url = detail_url(activity.id)
+        res = self.client.put(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        activity.refresh_from_db()
+        for k, v in payload.items():
+            self.assertEqual(getattr(activity, k), v)
+        self.assertEqual(activity.user, self.user)
+
+
+    def test_update_user_returns_error(self):
+        """Test changing the activity author returns an error"""
+        new_user = get_user_model().objects.create_user(
+            email='user2@example.com',
+            password='test1234',
+            is_staff=True,
+        )
+
+        activity = create_activity(user=self.user)
+
+        payload = {
+            'user': new_user.id,
+        }
+
+        url = detail_url(activity.id)
+        self.client.patch(url, payload)
+
+        activity.refresh_from_db()
+        self.assertEqual(activity.user, self.user)
+
+    def test_delete_activity(self):
+        """Test deleting an activity is successful"""
+        activity = create_activity(user=self.user)
+
+        url = detail_url(activity.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Activity.objects.filter(id=activity.id).exists())
